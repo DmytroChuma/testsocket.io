968ea3ff51484ee22aba5ef4f24ab6c1ef420b9e
DmytroChuma <73658961+DmytroChuma@users.noreply.github.com>
2022-01-22 16:38:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/testserver.html');
+  res.sendFile(__dirname + '/test.html');
 });
 
 io.on('connection', (socket) => {
